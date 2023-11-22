84b3f6499a909e6fcdbd061bf8b28350c9a9f2d3
Ivan <i85chiosa@gmail.com>
2023-11-22 18:31:21 +0100

    First commit

---
+++ ffm-java-23-2-spring-project/src/main/java/com/example/ffmjava232springproject/FfmJava232SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ffmjava232springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfmJava232SpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfmJava232SpringProjectApplication.class, args);
+    }
+
+}
---
+++ ffm-java-23-2-spring-project/src/test/java/com/example/ffmjava232springproject/FfmJava232SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ffmjava232springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfmJava232SpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
