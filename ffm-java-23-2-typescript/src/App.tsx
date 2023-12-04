77ae381870f5c9a149a1aa070dd25f0dadd3a187
Ivan <i85chiosa@gmail.com>
2023-12-04 14:03:34 +0100

    Typescript

--- ffm-java-23-2-typescript/src/App.tsx
+++ ffm-java-23-2-typescript/src/App.tsx
@@ -1,35 +1,34 @@
-
 import './App.css'
 
 function App() {
 
-   const age: number = 25;
+    const age: number = 25;
 
-   for(let i: number = 1; i <= age; i++) {
-       if(age > 18) {
-           console.log("You are an adult + 18");
-       } else {
-           console.log("You are not an adult!");
-       }
-   }
+    for (let i: number = 1; i <= age; i++) {
+        if (age > 18) {
+            console.log("You are an adult + 18");
+        } else {
+            console.log("You are not an adult!");
+        }
+    }
 
-   const score: number = 0;
-   if(score !== 0) {
-       console.log("Score is available");
-   }
+    const score: number = 0;
+    if (score !== 0) {
+        console.log("Score is available");
+    }
 
-   if(score) {
-       console.log("Score is evaluated as truthy.");
-   } else {
-       console.log('Score is not evaluated as truthy.');
-   }
+    if (score) {
+        console.log("Score is evaluated as truthy.");
+    } else {
+        console.log('Score is not evaluated as truthy.');
+    }
 
-   const username: string = " ";
-   if(username) {
-       console.log("Username is available.");
-   } else {
-       console.log("Username is not available.");
-   }
+    const username: string = " ";
+    if (username) {
+        console.log("Username is available.");
+    } else {
+        console.log("Username is not available.");
+    }
 
     if (!username) {
         console.log('Username is evaluated as falsy.');
@@ -38,13 +37,13 @@ function App() {
     }
 
     const isAdmin: boolean = false;
-    if(isAdmin) {
+    if (isAdmin) {
         console.log("isAdmin is evaluated as truthy");
     } else {
         console.log("isAdmin is evaluated not as truthy");
     }
 
-    if(!isAdmin) {
+    if (!isAdmin) {
         console.log("isAdmin is evaluated as falsy");
     }
 
@@ -68,13 +67,13 @@ function App() {
 // Call the function with n = 5
     drawChristmasTree(5);
 
-
     return (
-    <>
-        <h1>Hello World</h1>
-     <h1>{age}</h1>
-     <h1>{isAdmin}</h1>
-    </>
-  )
+        <>
+            <h1>Hello World</h1>
+            <h1>{age}</h1>
+            <h1>{isAdmin}</h1>
+        </>
+    )
 }
+
 export default App
